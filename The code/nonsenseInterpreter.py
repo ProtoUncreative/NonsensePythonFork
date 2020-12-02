99439582387f78d40ee00e0a3ca5b9b74e5f47c0
ProtoUncreative <InvertedGoldMic@outlook.com>
2020-12-02 16:59:54 +0330

    Revert "Update nonsenseInterpreter.py"
    This reverts commit a6ef08f1aef81818f7fca2babe562a37d0e35558.

--- The code/nonsenseInterpreter.py
+++ The code/nonsenseInterpreter.py
@@ -3,49 +3,46 @@
 from collections import deque
 import time
 
-print("Do Ctrl+C to stop the program.")
+ACC = 0
 
-while True:
-    ACC = 0
+UI = input("Commands: ")
+print("Processing..")
 
-    UI = input("Commands: ")
-    print("Processing..")
+DUI = deque(UI)
 
-    DUI = deque(UI)
+IP = 0
 
-    IP = 0
+fin = str()
 
-    fin = str()
+while IP!=len(DUI):
+    x=DUI[IP]
+    if x=="+":
+        ACC+=1
+    elif x=="-":
+        ACC-=1
+    elif x==".":
+        ACC=ACC
+    elif x=="#":
+        fin+=str(ACC)
+    elif x=="*":
+        ACC=ACC**2
+    elif x=="$":
+        fin+=chr(ACC)
+    elif x=="!":
+        ACC=0
+    elif x=="^":
+        ACC+=10
+    elif x=="&":
+        ACC-=10
+    elif x==",":
+        temp1 = str()
+        while len(temp1)!=1:
+            temp1=input("Please enter input (1 character long): ")
+        ACC=ord(temp1)
+    elif x=="}":
+        time.sleep(1)
+    elif x=="%":
+        IP=0
+    IP+=1
 
-    while IP!=len(DUI):
-        x=DUI[IP]
-        if x=="+":
-            ACC+=1
-        elif x=="-":
-            ACC-=1
-        elif x==".":
-            ACC=ACC
-        elif x=="#":
-            fin+=str(ACC)
-        elif x=="*":
-            ACC=ACC**2
-        elif x=="$":
-            fin+=chr(ACC)
-        elif x=="!":
-            ACC=0
-        elif x=="^":
-            ACC+=10
-        elif x=="&":
-            ACC-=10
-        elif x==",":
-            temp1 = str()
-            while len(temp1)!=1:
-                temp1=input("Please enter input (1 character long): ")
-            ACC=ord(temp1)
-        elif x=="}":
-            time.sleep(1)
-        elif x=="%":
-            IP=0
-        IP+=1
-
-    print(f"Output: {fin}")
+print(f"Output: {fin}")
